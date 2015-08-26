b4befb86c0fc7aa566cf94e9af2249a3009ef3c8
RobertZK <technoguyrob@gmail.com>
2015-08-26 10:24:31 -0500

    Test that nearZeroVar works

--- tests/testthat/test-caret.R
+++ tests/testthat/test-caret.R
@@ -6,3 +6,9 @@ test_that("it preserves caret function createDataPartition", {
   expect_identical({ set.seed(1); caret::createDataPartition(x, list = FALSE) },
     { set.seed(1); createDataPartition(x, list = FALSE) })
 })
+
+test_that("it preserves caret function nearZeroVar", {
+  expect_identical(caret::nearZeroVar(rep(0, 10)), nearZeroVar(rep(0, 10)))
+  expect_identical(caret::nearZeroVar(c(rep(0, 10), 0.1, 0.2)), nearZeroVar(c(rep(0, 10), 0.1, 0.2)))
+})
+
